5c75c7fb0d71808121a3abf9551a14f9e63000f6
srice2024 <srice@legacyforcemedia.com>
2025-08-03 17:46:49 -0700

    Edit - Adding in different DOM manipulations for reference material.

--- js_Dom.js
+++ js_Dom.js
@@ -1,1 +1,25 @@
 `use strict';`
+//Selecting different elements in the DOM
+console.log(document.querySelector('.message').textContent); //Reading the message
+document.querySelector('.message').textContent = 'correct number!'; //Changing the message
+console.log(document.querySelector('.message').textContent); //Reading the new message
+
+document.querySelector('.number').textContent = 13; //Changing the number
+document.querySelector('.score').textContent = 20; //Changing the score
+document.querySelector('.guess').value = 23; //Changing the guess input value
+console.log(document.querySelector('.guess').value); //Reading the guess input value
+
+
+//Common Dom manipulations
+document.querySelector('.number').textContent = '?'; //Display the ? in the UI
+document.querySelector('.guess').value = ''; //Reset the guess input value
+
+//Dom manipulation functions
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message; //Function to display the message
+};
+
+//Dom event button listeners
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess); //Reading the guess input value on button click
