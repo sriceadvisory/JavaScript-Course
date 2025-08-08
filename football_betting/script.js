7b4bdd4c6677ed6981cccc45bf5be04eedda3cd8
srice2024 <srice@legacyforcemedia.com>
2025-08-07 20:11:04 -0700

    Creation - creating base files for a eurpean football betting application.

---
+++ football_betting/script.js
@@ -0,0 +1,35 @@
+'use strict';
+const game = {
+    team1: 'NY Redbull',
+    team2: 'Charlotte FC',
+    players: [
+        [
+            'Davis',
+            'Peter',
+            'Barry',
+            'William',
+            'Martinez',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandoski',
+            'Kimmich',
+            'Alaba',
+            'Goretzka',
+            'Davies',
+        ],
+        [
+            'Buri',
+            'Schultz',
+            'Varun',
+            'Pryam',
+            'Steve',
+            'Fred',
+            'Hummel',
+            'Martin',
+            'Brent',
+            'Alshon',
+            'Sydney',
+        ]
+    ]
+}
